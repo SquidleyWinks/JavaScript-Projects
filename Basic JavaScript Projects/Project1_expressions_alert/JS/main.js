2b3bd6cca800f57853fb0d1a27a60583eb1cd266
Phil Uebbing <phil.uebbing@newcoursecc.com>
2023-01-16 17:00:21 -0600

    changes

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,20 +2,13 @@ var Y = "\"Test test test\""; // Assigning a variable
 
 window.alert(Y); // Creating an alert window
 
-document.write("Poops "); //Havin fun
-
-var X = "String a ding ding "; // Don't judge me -- it's been a long day
-
-document.write(X);
-
-document.write("Poops " + X);
-
-var Z = " something something smart sounding " + X;
-
-document.write(Z);
-
 var JLA = "The Justice League of America", S = "Superman", B = "Batman", W ="Wonder Woman", G = "Green Lantern", F = "The Flash", A ="Aquaman"; // Staying "On Brand"
 
 document.write(G); // Favorites' First
 
 3 + 3; // As far as I can tell, this doesn't do a damn thing...
+
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end ot the string";
+
+document.write(Sent1 + Sent2); // I do believe this completes the "Write an expression" part of the assignment
