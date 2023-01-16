c870573ae1782b74e30ec0074196bebe7cba92fe
Phil Uebbing <phil.uebbing@newcoursecc.com>
2023-01-16 16:54:24 -0600

    push

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+var Y = "\"Test test test\""; // Assigning a variable
+
+window.alert(Y); // Creating an alert window
+
+document.write("Poops "); //Havin fun
+
+var X = "String a ding ding "; // Don't judge me -- it's been a long day
+
+document.write(X);
+
+document.write("Poops " + X);
+
+var Z = " something something smart sounding " + X;
+
+document.write(Z);
+
+var JLA = "The Justice League of America", S = "Superman", B = "Batman", W ="Wonder Woman", G = "Green Lantern", F = "The Flash", A ="Aquaman"; // Staying "On Brand"
+
+document.write(G); // Favorites' First
+
+3 + 3; // As far as I can tell, this doesn't do a damn thing...
