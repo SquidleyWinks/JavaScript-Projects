6b7a46e978dba7d36a0e7a7c268ba5b1bda68817
Phil Uebbing <phil.uebbing@newcoursecc.com>
2023-02-03 16:41:34 -0600

    comments

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,12 +1,12 @@
 function countdown() {
-    var seconds = document.getElementById("seconds").value;
+    var seconds = document.getElementById("seconds").value; // Gets the variable value from the input
 
     function tick() {
-        seconds = seconds - 1;
+        seconds = seconds - 1; // Counts down
         TimeRanges.innerHTML = seconds;
         var time = setTimeout(tick, 1000);
         if (seconds == -1) {
-            alert("Time's up!");
+            alert("Time's up!"); // Alert when time is up
             clearTimeout(time);
             timer.innerHTML = "";
         }
@@ -14,28 +14,28 @@ function countdown() {
     tick();
 }
 
-let slideIndex = 1;
+let slideIndex = 1; // begins slideshow at first picture
 showSlides(slideIndex);
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides(slideIndex += n); // increases/decreases slides
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides(slideIndex = n); // sets displayed slide to n
 }
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    let slides = document.getElementsByClassName("mySlides"); // gets slides from HTML
+    let dots = document.getElementsByClassName("dot"); // gets dots from HTML
+    if (n > slides.length) {slideIndex = 1} // if we go past 3, loops back to 1
+    if (n < 1) {slideIndex = slides.length} // if we go past 1, loops back to 3
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" active", ""); // uses classes to indicate which image is currently shown
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
